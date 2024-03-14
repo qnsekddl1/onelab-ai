5bab54c170ba3131b75e0ab35dd5b58b098b1057
[qnsekddl1] <[qnsekddl1@gmail.com]>
2024-03-14 09:48:58 +0900

    models fix

--- reply/views.py
+++ reply/views.py
@@ -1,3 +1,5 @@
+from django.db.models import F
+from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,11 +8,8 @@ from reply.models import Reply
 
 class ReplyWriteAPI(APIView):
     def post(self, request):
-        # 1. FormData객체가 들어오는 가
-        # request.POST
-
-        # 2. JSON이 들어오는 가
         data = request.data
+
         data = {
             'reply_content': data['reply_content'],
             'community_id': data['community_id'],
@@ -18,4 +17,33 @@ class ReplyWriteAPI(APIView):
         }
 
         Reply.objects.create(**data)
+        return Response('success')
+
+
+class ReplyListAPI(APIView):
+    def get(self, request, community_id, page):
+        row_count = 5
+        offset = (page - 1) * row_count
+        limit = page * row_count
+
+        replies = Reply.objects.filter(community_id=community_id)\
+            .annotate(member_name=F('member__member_name'))\
+            .values('reply_content', 'id', 'member_name', 'created_date', 'member_id')
+
+        return Response(replies[offset:limit])
+
+class ReplyAPI(APIView):
+    def delete(self, request, reply_id):
+        Reply.objects.filter(id=reply_id).delete()
+        return Response('success')
+
+    def patch(self, request, reply_id):
+        reply_content = request.data['reply_content']
+        updated_date = timezone.now()
+
+        reply = Reply.objects.get(id=reply_id)
+        reply.reply_content = reply_content
+        reply.updated_date = updated_date
+        reply.save(update_fields=['reply_content', 'updated_date'])
+
         return Response('success')
--- school/models.py
+++ school/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from member.models import Member
 from oneLabProject.models import Period
 
-class School(models.Model):
-    member = models.ForeignKey(Member, on_delete=models.PROTECT, null=False)
+class School(Period):
     school_member_address = models.CharField(max_length=500, null=False, blank=False, default='서울시 강남구')
     school_name = models.CharField(null=False, blank=False, max_length=100)
+    member = models.ForeignKey(Member, primary_key=True, on_delete=models.PROTECT, null=False)
 
     class Meta:
         db_table = 'tbl_school'
-        ordering = ['-id']
+        ordering = ['-created_date']
